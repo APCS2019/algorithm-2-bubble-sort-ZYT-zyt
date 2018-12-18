90e64acd7944406bda5faded1f3d4c90ff944a74
TImZhou <45361581+ZYT-zyt@users.noreply.github.com>
2018-12-18 09:26:06 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,13 @@
-
+public static void maopao(int[] a) {
+    int l = a.length;
+    while (l > 0) {
+        for (int i = 0; i < l - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+        l--;
+    }
+}
